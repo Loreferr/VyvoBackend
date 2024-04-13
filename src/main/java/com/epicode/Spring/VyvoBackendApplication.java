e146dc07d0ac7ccf2bc620835f42d931d1f6aea9
Loreferr <lf.95@outlook.com>
2024-04-13 16:36:28 +0200

    first commit

--- src/main/java/com/epicode/Spring/VyvoBackendApplication.java
+++ src/main/java/com/epicode/Spring/VyvoBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WitApplication {
+public class VyvoBackendApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(WitApplication.class, args);
+		SpringApplication.run(VyvoBackendApplication.class, args);
 	}
 }
--- src/main/java/com/epicode/Spring/security/service/AuthService.java
+++ src/main/java/com/epicode/Spring/security/service/AuthService.java
@@ -1,6 +1,5 @@
 package com.epicode.Spring.security.service;
 
-import com.epicode.Spring.security.payload.EditProfileDto;
 import com.epicode.Spring.security.payload.LoginDto;
 import com.epicode.Spring.security.payload.RegisterDto;
 
@@ -9,7 +8,5 @@ public interface AuthService {
 	String login(LoginDto loginDto);
 
 	String register(RegisterDto registerDto);
-
-	String editprofile(EditProfileDto editprofileDto);
 
 }
